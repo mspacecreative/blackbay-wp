52371177ae2774f1ae5c354f66ad66d80b954669
Matt <matt@mspacecreative.com>
2018-06-28 14:14:12 -0300

    c

---
+++ wp-content/themes/blackbay/single.php
@@ -0,0 +1,350 @@
+<?php
+
+get_header();
+
+$show_default_title = get_post_meta( get_the_ID(), '_et_pb_show_title', true );
+
+$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
+
+?>
+
+<div id="main-content">
+	<?php
+		if ( et_builder_is_product_tour_enabled() ):
+			// load fullwidth page in Product Tour mode
+			while ( have_posts() ): the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+					<?php
+						the_content();
+					?>
+					</div> <!-- .entry-content -->
+
+				</article> <!-- .et_pb_post -->
+
+		<?php endwhile;
+		else:
+	?>
+	<div class="container">
+		<div id="content-area" class="clearfix">
+			<div id="left-area">
+			<style type="text/css">
+			
+			.acf-map {
+				width: 100%;
+				height: 400px;
+				border: #ccc solid 1px;
+				margin: 20px 0;
+			}
+			
+			/* fixes potential theme css conflict */
+			.acf-map img {
+			   max-width: inherit !important;
+			}
+			
+			</style>
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMjm_0er-ybuWcVoPXYLSr82mn6bvNqCY"></script>
+			<script type="text/javascript">
+			(function($) {
+			
+			/*
+			*  new_map
+			*
+			*  This function will render a Google Map onto the selected jQuery element
+			*
+			*  @type	function
+			*  @date	8/11/2013
+			*  @since	4.3.0
+			*
+			*  @param	$el (jQuery element)
+			*  @return	n/a
+			*/
+			
+			function new_map( $el ) {
+				
+				// var
+				var $markers = $el.find('.marker');
+				
+				
+				// vars
+				var args = {
+					zoom		: 16,
+					center		: new google.maps.LatLng(0, 0),
+					mapTypeId	: google.maps.MapTypeId.ROADMAP
+				};
+				
+				
+				// create map	        	
+				var map = new google.maps.Map( $el[0], args);
+				
+				
+				// add a markers reference
+				map.markers = [];
+				
+				
+				// add markers
+				$markers.each(function(){
+					
+			    	add_marker( $(this), map );
+					
+				});
+				
+				
+				// center map
+				center_map( map );
+				
+				
+				// return
+				return map;
+				
+			}
+			
+			/*
+			*  add_marker
+			*
+			*  This function will add a marker to the selected Google Map
+			*
+			*  @type	function
+			*  @date	8/11/2013
+			*  @since	4.3.0
+			*
+			*  @param	$marker (jQuery element)
+			*  @param	map (Google Map object)
+			*  @return	n/a
+			*/
+			
+			function add_marker( $marker, map ) {
+			
+				// var
+				var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+			
+				// create marker
+				var marker = new google.maps.Marker({
+					position	: latlng,
+					map			: map
+				});
+			
+				// add to array
+				map.markers.push( marker );
+			
+				// if marker contains HTML, add it to an infoWindow
+				if( $marker.html() )
+				{
+					// create info window
+					var infowindow = new google.maps.InfoWindow({
+						content		: $marker.html()
+					});
+			
+					// show info window when marker is clicked
+					google.maps.event.addListener(marker, 'click', function() {
+			
+						infowindow.open( map, marker );
+			
+					});
+				}
+			
+			}
+			
+			/*
+			*  center_map
+			*
+			*  This function will center the map, showing all markers attached to this map
+			*
+			*  @type	function
+			*  @date	8/11/2013
+			*  @since	4.3.0
+			*
+			*  @param	map (Google Map object)
+			*  @return	n/a
+			*/
+			
+			function center_map( map ) {
+			
+				// vars
+				var bounds = new google.maps.LatLngBounds();
+			
+				// loop through all markers and create bounds
+				$.each( map.markers, function( i, marker ){
+			
+					var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+			
+					bounds.extend( latlng );
+			
+				});
+			
+				// only 1 marker?
+				if( map.markers.length == 1 )
+				{
+					// set center of map
+				    map.setCenter( bounds.getCenter() );
+				    map.setZoom( 16 );
+				}
+				else
+				{
+					// fit to bounds
+					map.fitBounds( bounds );
+				}
+			
+			}
+			
+			/*
+			*  document ready
+			*
+			*  This function will render each map when the document is ready (page has loaded)
+			*
+			*  @type	function
+			*  @date	8/11/2013
+			*  @since	5.0.0
+			*
+			*  @param	n/a
+			*  @return	n/a
+			*/
+			// global var
+			var map = null;
+			
+			$(document).ready(function(){
+			
+				$('.acf-map').each(function(){
+			
+					// create map
+					map = new_map( $(this) );
+			
+				});
+			
+			});
+			
+			})(jQuery);
+			</script>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if (et_get_option('divi_integration_single_top') <> '' && et_get_option('divi_integrate_singletop_enable') == 'on') echo(et_get_option('divi_integration_single_top')); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+					<?php if ( ( 'off' !== $show_default_title && $is_page_builder_used ) || ! $is_page_builder_used ) { ?>
+						<div class="et_post_meta_wrapper">
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+
+						<?php
+							if ( ! post_password_required() ) :
+
+								et_divi_post_meta();
+
+								$thumb = '';
+
+								$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
+
+								$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
+								$classtext = 'et_featured_image';
+								$titletext = get_the_title();
+								$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
+								$thumb = $thumbnail["thumb"];
+
+								$post_format = et_pb_post_format();
+
+								if ( 'video' === $post_format && false !== ( $first_video = et_get_first_video() ) ) {
+									printf(
+										'<div class="et_main_video_container">
+											%1$s
+										</div>',
+										$first_video
+									);
+								} else if ( ! in_array( $post_format, array( 'gallery', 'link', 'quote' ) ) && 'on' === et_get_option( 'divi_thumbnails', 'on' ) && '' !== $thumb ) {
+									print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
+								} else if ( 'gallery' === $post_format ) {
+									et_pb_gallery_images();
+								}
+							?>
+
+							<?php
+								$text_color_class = et_divi_get_post_text_color();
+
+								$inline_style = et_divi_get_post_bg_inline_style();
+
+								switch ( $post_format ) {
+									case 'audio' :
+										$audio_player = et_pb_get_audio_player();
+
+										if ( $audio_player ) {
+											printf(
+												'<div class="et_audio_content%1$s"%2$s>
+													%3$s
+												</div>',
+												esc_attr( $text_color_class ),
+												$inline_style,
+												$audio_player
+											);
+										}
+
+										break;
+									case 'quote' :
+										printf(
+											'<div class="et_quote_content%2$s"%3$s>
+												%1$s
+											</div> <!-- .et_quote_content -->',
+											et_get_blockquote_in_content(),
+											esc_attr( $text_color_class ),
+											$inline_style
+										);
+
+										break;
+									case 'link' :
+										printf(
+											'<div class="et_link_content%3$s"%4$s>
+												<a href="%1$s" class="et_link_main_url">%2$s</a>
+											</div> <!-- .et_link_content -->',
+											esc_url( et_get_link_url() ),
+											esc_html( et_get_link_url() ),
+											esc_attr( $text_color_class ),
+											$inline_style
+										);
+
+										break;
+								}
+
+							endif;
+						?>
+					</div> <!-- .et_post_meta_wrapper -->
+				<?php  } ?>
+
+					<div class="entry-content">
+					<?php
+						do_action( 'et_before_content' );
+
+						the_content();
+
+						wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
+					?>
+					</div> <!-- .entry-content -->
+					<div class="et_post_meta_wrapper">
+					<?php
+					if ( et_get_option('divi_468_enable') == 'on' ){
+						echo '<div class="et-single-post-ad">';
+						if ( et_get_option('divi_468_adsense') <> '' ) echo( et_get_option('divi_468_adsense') );
+						else { ?>
+							<a href="<?php echo esc_url(et_get_option('divi_468_url')); ?>"><img src="<?php echo esc_attr(et_get_option('divi_468_image')); ?>" alt="468" class="foursixeight" /></a>
+				<?php 	}
+						echo '</div> <!-- .et-single-post-ad -->';
+					}
+				?>
+
+					<?php if (et_get_option('divi_integration_single_bottom') <> '' && et_get_option('divi_integrate_singlebottom_enable') == 'on') echo(et_get_option('divi_integration_single_bottom')); ?>
+
+					<?php
+						if ( ( comments_open() || get_comments_number() ) && 'on' == et_get_option( 'divi_show_postcomments', 'on' ) ) {
+							comments_template( '', true );
+						}
+					?>
+					</div> <!-- .et_post_meta_wrapper -->
+				</article> <!-- .et_pb_post -->
+
+			<?php endwhile; ?>
+			</div> <!-- #left-area -->
+
+			<?php get_sidebar(); ?>
+		</div> <!-- #content-area -->
+	</div> <!-- .container -->
+	<?php endif; ?>
+</div> <!-- #main-content -->
+
+<?php
+
+get_footer();
