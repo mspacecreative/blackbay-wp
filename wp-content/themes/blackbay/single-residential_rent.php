47b36364a235a2078414227e613c072eec2f1b65
Matt <matt@mspacecreative.com>
2018-07-04 10:50:03 -0300

    c

--- wp-content/themes/blackbay/single-residential_rent.php
+++ wp-content/themes/blackbay/single-residential_rent.php
@@ -166,7 +166,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 				if( !empty($location) ):
 				?>
 				<div class="acf-map">
-					<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+					<?php echo $location; ?>
 				</div>
 				<?php endif; ?>
 				<!-- END GOOGLE MAP -->
