34dc4ed48562b4a444d92532f5a82bfdc5d0218e
Matt <matt@mspacecreative.com>
2018-11-14 14:04:38 -0400

    c

--- wp-content/themes/blackbay/single-residential_for_sale.php
+++ wp-content/themes/blackbay/single-residential_for_sale.php
@@ -89,7 +89,9 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 						</table>
 						
 						<div class="button light">
-							<a href="#" class="view-more"><?php _e('View More'); ?></a>
+							<p>
+								<a href="#" class="view-more"><?php _e('View More'); ?></a>
+							</p>
 						</div>
 						
 						<?php endif; ?>
