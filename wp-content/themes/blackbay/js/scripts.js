5ecca9724613a5b3c8d92cfc289c21a6862c12bc
Matt <matt@mspacecreative.com>
2018-11-14 17:52:55 -0400

    c

--- wp-content/themes/blackbay/js/scripts.js
+++ wp-content/themes/blackbay/js/scripts.js
@@ -3,7 +3,7 @@ $(function () {
 	$('table.bullet-box').each(function(){
 	    var max = 4
 	    if ($(this).find('tr').length > max) {
-	        $(this).find('tr:gt('+max+')').hide().end().append('<tr class="sub_accordian"><td><div class="show_more button light"><a href="javascript:void(0);">View More</a></div></td></tr>');
+	        $(this).find('tr:gt('+max+')').hide().end().append('<tr class="sub_accordian"><td colspan="2" style="border-top: 2px solid #333;"><div class="show_more button light"><a href="javascript:void(0);">View More</a></div></td></tr>');
 	        $('.sub_accordian').click( function(){
 	            $(this).siblings(':gt('+max+')').toggle();
 	            if ( $('.show_more').length ) {
