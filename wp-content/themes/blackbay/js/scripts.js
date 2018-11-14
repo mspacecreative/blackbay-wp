2bf1e9e846cfa7dc71398eb67c0759b683eb5aa4
Matt <matt@mspacecreative.com>
2018-11-14 17:58:41 -0400

    c

--- wp-content/themes/blackbay/js/scripts.js
+++ wp-content/themes/blackbay/js/scripts.js
@@ -7,8 +7,10 @@ $(function () {
 	        $('.sub_accordian').click( function(){
 	            $(this).siblings(':gt('+max+')').toggle();
 	            if ( $('.show_more').length ) {
-	                $(this).html('<tr class="sub_accordian"><td colspan="2" style="border-top: 2px solid #333;"><div class="show_more button light"><a href="javascript:void(0);">View Less</a></div></td></tr>');
-	            }
+	                $(this).html('<tr class="sub_accordian"><td colspan="2" style="border-top: 2px solid #333;"><div class="show_less button light"><a href="javascript:void(0);">View Less</a></div></td></tr>');
+	            } else {
+	                $(this).html('<tr class="sub_accordian"><td colspan="2" style="border-top: 2px solid #333;"><div class="show_more button light"><a href="javascript:void(0);">View More</a></div></td></tr>');
+	            };
 	        });
 	    };
 	});
