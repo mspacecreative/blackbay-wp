f7b4fb8a3dc9abbcd7866139ce6dd1e7cab03f25
Matt <matt@mspacecreative.com>
2018-11-14 15:02:58 -0400

    c

--- wp-content/themes/blackbay/js/scripts.js
+++ wp-content/themes/blackbay/js/scripts.js
@@ -19,12 +19,12 @@ $(function () {
 		
 		// VIEW MORE DETAILS BUTTON
 		$('.view-more').click(function() {
-			$(this).parent().parent().siblings('.bullet-box').children('tbody').children('tr').slideDown();
-			if ($('.bullet-box tr').css('display', 'table-row') == true) {
+			$(this).parent().parent().siblings('.bullet-box').children('tbody').children('tr').slideToggle();
+			/*if ($('.bullet-box tr').css('display', 'table-row') == true) {
 				$(this).parent().parent().siblings('.bullet-box').children('tbody').children('tr').slideUp();
 			} else {
 				$(this).parent().parent().siblings('.bullet-box').children('tbody').children('tr').slideDown();
-			}
+			}*/
 		});
 		
 		if ($('table.bullet-box').hasClass('reveal')) {
